3d228526871e72a940ca1ee3ed6d9e3dc2479421
ShirakoPonZ <maple8495@gmail.com>
2022-09-09 19:52:24 +0900

    上部幅決定

--- components/nav.js
+++ components/nav.js
@@ -5,25 +5,26 @@ import styles from "../styles/nav.module.scss";
 
 import Logo from "./logo";
 import FlexContainer from "./flexContainer";
+import Container from "./container";
 
 
 export default function Nav(){
     return(
     <>
-        <FlexContainer>
-            {/* ロゴ */}
-            <Logo />
-
-            <nav className={styles.menu}>
-                <ul>
-                    <li><Scroll to="/" smooth={true} duration={300} offset={0}>Top</Scroll></li>
-                    <li><Scroll to="clips" smooth={true} duration={300} offset={0}>clips</Scroll></li>
-                    <li><Link href="/music"><a>楽曲</a></Link></li>
-                    <li><Link href="/aniv"><a>ゆにチル記録</a></Link></li>
-                    <li><Scroll to="staffs" smooth={true} duration={300} offset={0}>staffs</Scroll></li>
-                </ul>
-            </nav>
-        </FlexContainer>
+        <Container large>
+            <FlexContainer>
+                <Logo />
+                <nav className={styles.menu}>
+                    <ul>
+                        <li><Scroll to="/" smooth={true} duration={300} offset={0}>Top</Scroll></li>
+                        <li><Scroll to="clips" smooth={true} duration={300} offset={0}>clips</Scroll></li>
+                        <li><Link href="/music"><a>楽曲</a></Link></li>
+                        <li><Link href="/aniv"><a>ゆにチル記録</a></Link></li>
+                        <li><Scroll to="staffs" smooth={true} duration={300} offset={0}>staffs</Scroll></li>
+                    </ul>
+                </nav>
+            </FlexContainer>
+        </Container>
     </>
     )
 }
@@ -32,6 +33,7 @@ export default function Nav(){
 export function NavPages(){
     return(
         <FlexContainer>
+            <Container large>
             {/* ロゴ */}
             <Logo />
 
@@ -44,6 +46,7 @@ export function NavPages(){
                     <li><Link href="/"><a>staffs</a></Link></li>
                 </ul>
             </nav>
+            </Container>
         </FlexContainer>
     )
 }
