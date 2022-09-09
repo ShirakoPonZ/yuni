2efb8996424078bd967675cb8debc312d68ba0ef
ShirakoPonZ <maple8495@gmail.com>
2022-09-09 19:36:26 +0900

    youtubeライブラリ導入

--- pages/music.js
+++ pages/music.js
@@ -1,7 +1,37 @@
+import { NavPages } from "../components/nav";
+import YouTube from "react-youtube";
+
 export default function Music(){
     return(
         <>
-            music
+            <NavPages />
+
+            <div className="">
+                <h1>music</h1>
+
+                <h2>Original</h2>
+
+
+                <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+                    <div>
+                        <h2>曲名</h2>
+                        <p>本文</p>
+                    </div>
+
+                    <YouTube videoId="kHk9qBGTnh8" />
+                </div>
+                <a href="#">サブスクリンク</a>
+
+
+                <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+                    <div>
+                        <h2>曲名</h2>
+                        <p>本文</p>
+                    </div>
+
+                    <YouTube videoId="LjkDJHk2KrE" />
+                </div>
+            </div>
         </>
     )
 }
