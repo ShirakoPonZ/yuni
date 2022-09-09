ca2d2b2663ca866eadde59e49bd13ed52ee9bdf2
ShirakoPonZ <maple8495@gmail.com>
2022-09-09 21:14:23 +0900

    コンテナ―準備

--- components/flexContainer.js
+++ components/flexContainer.js
@@ -1,8 +1,28 @@
 import styles from "../styles/flexContainer.module.scss";
 
+// デフォルト
 export default function FlexContainer({ children }){
     return(
         <div className={styles.flexContainer}>
+            {children}
+        </div>
+    )
+}
+
+
+// Linksコンテナ―
+export function LinkContainer({ children }){
+    return(
+        <div className={styles.linksContainer}>
+            {children}
+        </div>
+    )
+}
+
+// Clipsコンテナ― 
+export function ClipsContainer({ children }){
+    return(
+        <div className={styles.clipsContainer}>
             {children}
         </div>
     )
--- components/links.js
+++ components/links.js
@@ -2,6 +2,8 @@ import styles from "../styles/links.module.scss";
 
 import Image from "next/image";
 
+import { LinkContainer } from "./flexContainer";
+
 import youtubeIcon from "../img/youtube_logo.png";
 import twitterIcon from "../img/twitter_logo.png";
 import tiktokIcon from "../img/tiktok_logo.svg";
@@ -12,51 +14,58 @@ export default function Links() {
     return(
     <>
         <h1>Links</h1>
-        <div className={styles.links}>
-            <a target="_blank" rel="noreferrer" href="https://www.youtube.com/c/YuNivirtualsinger">
-                <figure>
-                <Image
-                    src={youtubeIcon}
-                    layout="responsive"
-                    alt="Youtubeアイコン"
-                    objectFit
-                />
-                </figure>
-            </a>
+        <LinkContainer>
 
-            <a target="_blank" rel="noreferrer" href="https://twitter.com/_YuNiofficial_">
-            <figure>
-                <Image
-                    src={twitterIcon}
-                    layout="responsive"
-                    alt="Twitterアイコン"
-                    objectFit
-                />
-                </figure>
-            </a>
-            
-            <a target="_blank" rel="noreferrer" href="https://yunionwave.booth.pm/">
-                <figure>
-                <Image
-                    src={boothIcon}
-                    layout="responsive"
-                    alt="boothアイコン"
-                />
-                </figure>
-            </a>
-            
-            <a target="_blank" rel="noreferrer" href="https://www.tiktok.com/@yuni_officialchannel_">
-                <figure>
-                <Image
-                    src={tiktokIcon}
-                    layout="responsive"
-                    alt="tiktokアアイコン"
-                />
-                </figure>
-            </a>
+            <div className="firstColumn">
+                <a target="_blank" rel="noreferrer" href="https://www.youtube.com/c/YuNivirtualsinger">
+                    <figure>
+                        <Image
+                            src={youtubeIcon}
+                            layout="responsive"
+                            alt="Youtubeアイコン"
+                            objectFit
+                        />
+                    </figure>
+
+
+                <a target="_blank" rel="noreferrer" href="https://twitter.com/_YuNiofficial_">
+                    <figure>
+                        <Image
+                            src={twitterIcon}
+                            layout="responsive"
+                            alt="Twitterアイコン"
+                            objectFit
+                        />
+                    </figure>
+                </a>
+                </a>
+            </div>
+
+
+            <div className="secondColumn">
+                <a target="_blank" rel="noreferrer" href="https://yunionwave.booth.pm/">
+                    <figure>
+                    <Image
+                        src={boothIcon}
+                        layout="responsive"
+                        alt="boothアイコン"
+                    />
+                    </figure>
+                </a>
+
+                <a target="_blank" rel="noreferrer" href="https://www.tiktok.com/@yuni_officialchannel_">
+                    <figure>
+                    <Image
+                        src={tiktokIcon}
+                        layout="responsive"
+                        alt="tiktokアアイコン"
+                    />
+                    </figure>
+                </a>
+            </div>
 
             <a target="_blank" rel="noreferrer" href="https://yunionwave.com/">YuNiオフィシャルサイト</a>
-        </div>
+        </LinkContainer>
     </>
 
     )
