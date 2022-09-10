ac88960f2d939bf0bc67ab80a7b49e8834e70320
ShirakoPonZ <maple8495@gmail.com>
2022-09-10 15:41:40 +0900

    レイアウトスタイル変更

--- pages/aniv.js
+++ pages/aniv.js
@@ -1,5 +1,7 @@
+import styles from "../styles/aniv.module.scss";
+
 import Image from "next/image";
-import ilandImg from "../img/yuni_laird.jpg"
+import ilandImg from "../img/yuni_laird.jpg";
 import { NavPages } from "../components/nav";
 import Container from "../components/container";
 
@@ -10,31 +12,35 @@ export default function Aniv(){
         <>
             <NavPages />
             <Container>
-            {/* 二列スタイルを適用 */}
-            <div style={{ display: "flex" }}>
-                <figure style={{ width: "50vw", height: "400px", backgroundColor: "blue" }}>
-                    <Image
-                        src={ilandImg}
-                        layout="responsive"
-                        alt="贈呈書"
-                    />
-                </figure>
+                {/* 二列スタイルを適用 */}
+                <div className={styles.sideBySideCenter}>
+                    <figure>
+                        <Image
+                            src={ilandImg}
+                            layout="responsive"
+                            alt="贈呈書"
+                        />
+                    </figure>
+                    <div>
+                        <h1>♪YuNiBirth記念企画♪</h1>
+
+                        <p>誕生日を祝して「スコットランド デュナンズ城城主の権利」を進呈します。</p>
+                        <p>制定日: 2022年10月1日</p>
+                        <p>今後のご健康とご多幸を、心よりお祈り申し上げます。</p>
+                    </div>
+                </div>
+
+                <div style={{
+                    width: "200px",
+                    height: "100px",
+                    margin: "0 auto"
+                }}>
+                    バナー
+                </div>
 
                 <div>
-                    {/* <h1>♪YuNiBirth記念企画♪</h1>
-
-                    <p>誕生日を祝して「スコットランド デュナンズ城城主の権利」を進呈します。</p>
-                    <p>制定日: 2022年10月1日</p>
-                    <p>今後のご健康とご多幸を、心よりお祈り申し上げます。</p> */}
+                    <h1>ゆにチル活動記録</h1>
                 </div>
-            </div>
-
-            <div>
-                画像入る
-            </div>
-
-            <div>
-            </div>
             </Container>
         </>
     )
